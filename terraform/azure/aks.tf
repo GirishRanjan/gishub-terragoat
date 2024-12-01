6258cea12ae21ae8eaa9fb621944eb51a243f47a
GirishRanjan <134987016+GirishRanjan@users.noreply.github.com>
2024-12-01 10:27:17 +0800

    Update aks.tf - Test4

--- terraform/azure/aks.tf
+++ terraform/azure/aks.tf
@@ -1,4 +1,5 @@
 resource azurerm_kubernetes_cluster "k8s_cluster" {
+# Test case for check skip via comment
 # checkov:skip=CKV_AZURE_4: not needed
 # checkov:skip=CKV_AZURE_170: not required
 # checkov:skep=CKV_AZURE_115: not necessary
